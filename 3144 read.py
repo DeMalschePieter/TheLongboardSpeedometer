7f2f48d4d511d7aad729abf5b13e634b25561fa7
PieterDeMalsche <pieter.de.malsche@student.howest.be>
2017-06-13 13:30:37 +0200

    Verwijderen onnodige comment lines

--- 3144 read.py
+++ 3144 read.py
@@ -10,49 +10,6 @@ GPIO.setup(sensor, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 lcd.lcd_init()
 
 
-# vorigeWaarde = 1
-# aantalkeer = 0
-# aantalMeter = 0
-#
-#
-# start = default_timer()
-# secondtimer = default_timer()
-
-# def perseconde():
-#     aantalMeter2 = 0
-#     sensoor = GPIO.input(sensor)
-#     secondentijd = default_timer() - start
-#
-#     while secondentijd <= 1:
-#         secondentijd = default_timer() - start
-#         if sensoor != vorigeWaarde:
-#             aantalMeter2 += 0.1
-#
-#         voorigewaarde = sensoor
-#     return aantalMeter2 * 3.6
-#
-#
-# def meterPerSeconde():
-#     vorigeWaarde = 1
-#     aantalkeer = 0
-#     aantalMeter = 0
-#     sensorwaarde = GPIO.input(12)
-#
-#
-#     tijd1 = time.time()
-#     tijd2 = time.time()
-#     while (tijd2 - tijd1) <= 1:
-#         if sensorwaarde != vorigeWaarde:
-#             aantalkeer += 0.5
-#             aantalMeter += 0.1
-#
-#
-#         vorigeWaarde = sensorwaarde
-#         tijd2 = time.time()
-#
-#     return aantalMeter
-
-
 def meterPerSecondeKenneth():
     omwenteligen = 0
     vorigeWaarde = 1
@@ -83,65 +40,6 @@ def meterPerSecondeKenneth():
 try:
 
     while True:
-
-
-        # aantalSeconden = default_timer() - start
-        # sensorwaarde = GPIO.input(sensor)
-        #
-        #
-        #
-        # if sensorwaarde != vorigeWaarde:
-        #     aantalkeer += 0.5
-        #     aantalMeter += 0.1
-        #
-        #
-        # vorigeWaarde = sensorwaarde
-
-
-
-        # Print out results
-
-
-        # sensoor = GPIO.input(sensor)
-        # secondentijd = default_timer() - start
-        #
-        #
-        # while secondentijd <= 1:
-        #     secondentijd = default_timer() - start
-        #     if sensoor != vorigeWaarde:
-        #         aantalMeter2 += 0.1
-        #
-        #     voorigewaarde = sensoor
-
-
-        # omwenteligen = 0
-        # vorigeWaarde = 1
-        #
-        # tijd1 = time.time()
-        # tijd2 = time.time()
-        #
-        # while (tijd2 - tijd1) <= 1:
-        #
-        #     sensorwaarde = GPIO.input(4)
-        #
-        #     if vorigeWaarde != sensorwaarde:
-        #         omwenteligen += 1
-        #         vorigeWaarde = sensorwaarde
-        #
-        #     else:
-        #         vorigeWaarde = sensorwaarde
-        #
-        #     tijd2 = time.time()
-        #
-        #     # omtrek wiel 21,6769 cm
-        #
-        # str(round((((omwenteligen / 2) * 0.2167) * 3.6),2))
-
-
-
-
-
-
         tekst = round(meterPerSecondeKenneth(),2)
         lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
         lcd.lcd_byte(0x01, False)
@@ -159,8 +57,6 @@ try:
 
         # time.sleep(0.1)
         # print(duration)
-
-        # Wait before repeating loop
 except KeyboardInterrupt:
     GPIO.cleanup()
 
